9719fa0badd2a103a8d6f45dc91b0183ec2af02f
Darian Chance <darianfelixchance@gmail.com>
2026-05-12 21:34:22 -0600

    Add 6kyu: create phone number

---
+++ python/6kyu/create-phone-number.py
@@ -0,0 +1,21 @@
+# Kata: Create Phone Number
+# Difficulty: 6 kyu
+#
+# Description: 
+# Take an array of digits and return them properly formatted as a phone number, following the correct order of the array.   
+#
+# Approach:
+# Iterate through the array, concatenating numbers recast as strings with the formatting symbols in the correct positions. 
+
+def create_phone_number(n):
+    str_num = "("
+    count = 0
+    for num in n: 
+        str_num += str(num)
+        if count == 2:
+            str_num += ") "
+        if count == 5: 
+            str_num += "-"
+        count += 1
+    
+    return str_num
