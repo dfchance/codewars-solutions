c045277ed2385ed6da4eabd334d7d1863ecf2d4a
Darian Chance <darianfelixchance@gmail.com>
2026-05-14 23:57:02 -0600

    Refactored 6kyu: create phone number - single line f-string

--- python/6kyu/create_phone_number.py
+++ python/6kyu/create_phone_number.py
@@ -4,10 +4,11 @@
 # 
 # Description: 
 # Take an array of digits and return them properly formatted as a phone number, following the correct order of the array.   
-#
-# Approach:
-# Iterate through the array, concatenating numbers recast as strings with the formatting symbols in the correct positions. 
+# 
 
+# Original Solution from May 11, 2026
+# Approach:
+# Iterate through the array, concatenating numbers recast as strings with the formatting symbols in the correct positions.
 def create_phone_number(n):
     str_num = "("
     count = 0
@@ -20,3 +21,9 @@ def create_phone_number(n):
         count += 1
     
     return str_num
+
+# Refactored Solution from May 14, 2026
+# Approach:
+# Use f-string with direct indexing to format phone number in one line. 
+def create_phone_number(n):
+    return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
