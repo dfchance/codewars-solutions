f5451bdadcca7f81b9c093241ca48f43e3445d7e
Darian Chance <darianfelixchance@gmail.com>
2026-05-24 21:50:21 -0600

    Add 8kyu: convert boolean to yes or no

---
+++ python/8kyu/convert_boolean_to_yes_no.py
@@ -0,0 +1,12 @@
+# Kata: Convert Boolean Values to String 'Yes' or 'No'
+# Difficulty: 8 kyu
+# URL: https://www.codewars.com/kata/53369039d7ab3ac506000467/
+# 
+# Description: 
+# Complete a function that returns 'Yes' for True and 'No' for False.
+#
+# Approach:
+# Use an if/else statement in the return statement.  
+
+def bool_to_word(boolean):
+    return 'Yes' if boolean==True else 'No'
