7949d1aed91ad734d25f37f5f6becb39381947c0
Darian Chance <darianfelixchance@gmail.com>
2026-05-24 22:22:04 -0600

    Refactor 7kyu: credit card mask

--- python/7kyu/credit_card_mask.py
+++ python/7kyu/credit_card_mask.py
@@ -5,9 +5,10 @@
 # Description: 
 # Mask all but the last four digits of a passed string.
 #
+
+# Original Solution from May 12, 2026
 # Approach:
 # Return a string with all but the last 4 digits replaced by '#'. 
-
 def maskify(cc):
     l = len(cc)
     if l <= 4:
@@ -15,3 +16,9 @@ def maskify(cc):
     else:
         cc = "#" * (l - 4) + cc[(l-4):]
         return cc
+    
+# Refactored Solution from May 24, 2026
+# Approach:
+# Simplify into a single return statement using a concatenated string with all but the last 4 digits replaced by '#'. 
+def maskify(cc):
+    return cc if len(cc) <= 4 else "#" * (len(cc) - 4) + cc[-4:]
