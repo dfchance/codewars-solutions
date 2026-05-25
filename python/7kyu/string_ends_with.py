327d8a8cbb9d2e056f8263f278ea52146bac2448
Darian Chance <darianfelixchance@gmail.com>
2026-05-24 21:46:11 -0600

    Add 7kyu: string ends with?

---
+++ python/7kyu/string_ends_with.py
@@ -0,0 +1,12 @@
+# Kata: String Ends With? 
+# Difficulty: 7 kyu
+# URL: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+#
+# Description: 
+# Determine if the first string passed ends in the second string passed.
+#
+# Approach:
+# Use the string function endswith(str). 
+
+def solution(text, ending):
+    return text.endswith(ending)
